3a284d37a8cdcc821ef44bb0fb6c3112c3429352
lrhtony <tony07017@163.com>
2021-03-04 00:04:15 +0800

    fix 500
    Signed-off-by: lrhtony <tony07017@163.com>

--- api/b23.py
+++ api/b23.py
@@ -4,17 +4,20 @@ import base64
 app = Flask(__name__)
 
 @app.route('/api/b23')
-def post():
-    url = request.args.get('url')
-    url = base64.b64decode(url).decode("utf-8")
-    data = {
-        'build': 6180000,
-        'buvid': 'test',
-        'oid': url,
-        'platform': 'android',
-        'share_channel': 'COPY',
-        'share_id': 'public.webview.0.0.pv',
-        'share_mode': 3
-    }
-    html = requests.post("https://api.bilibili.com/x/share/click", data)
-    return html.text
+def b23():
+    try:
+        url = request.args.get('url')
+        url = base64.b64decode(url).decode("utf-8")
+        data = {
+            'build': 6180000,
+            'buvid': 'test',
+            'oid': url,
+            'platform': 'android',
+            'share_channel': 'COPY',
+            'share_id': 'public.webview.0.0.pv',
+            'share_mode': 3
+        }
+        html = requests.post("https://api.bilibili.com/x/share/click", data)
+        return html.text
+    except:
+        return 'Request error.'
