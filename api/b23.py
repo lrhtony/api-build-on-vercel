1b9c4f59d8dde15897f6341dba3bb23b0184e3f6
lrhtony <tony07017@163.com>
2021-03-19 21:10:47 +0800

    在服务端生成跳转链接

--- api/b23.py
+++ api/b23.py
@@ -6,18 +6,31 @@ app = Flask(__name__)
 @app.route('/api/b23')
 def b23():
     try:
-        url = request.args.get('url')
-        url = base64.b64decode(url).decode("utf-8")
+        inputUrl = request.args.get('url')
+        checkUrl = base64.b64decode(inputUrl).decode('utf-8')   #确保url参数合法
+    except:
+        return 'The parameter "url" is incorrect.'
+
+    if checkUrl[:7] == 'http://' or checkUrl[:8] == 'https://':
+        sendUrl = 'http.lrhtony.cn://.bilibili.com/jump.html?key=' + inputUrl   #处理最终链接
         data = {
             'build': 6180000,
             'buvid': 'test',
-            'oid': url,
+            'oid': sendUrl,
             'platform': 'android',
             'share_channel': 'COPY',
             'share_id': 'public.webview.0.0.pv',
             'share_mode': 3
         }
-        html = requests.post("https://api.bilibili.com/x/share/click", data)
-        return html.text
-    except:
-        return 'Request error.'
+        try:
+            html = requests.post("https://api.bilibili.com/x/share/click", data)  # 发送数据
+        except:
+            return 'Server request exception.'  #处理请求错误或超时
+
+        return html.text    #返回官方结果
+    else:
+        return 'The link should include the "http" or "https" protocol header.'    #筛选出不包含http/https请求头的链接
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
